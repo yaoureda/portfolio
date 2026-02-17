9199bd18ccf4557e33dddc0cb8e0f06e2f8d555b
YAOU Reda <yaoureda24@gmail.com>
2026-02-17 23:41:53 +0100

    making the app responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,38 @@
 import { NavLink } from "react-router-dom";
 import { FaHome, FaGraduationCap, FaFolderOpen } from "react-icons/fa";
+import { useState } from "react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav>
-      <ul>
-        <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}><FaHome /> Accueil</NavLink></li>
-        <li><NavLink to="/formation" className={({ isActive }) => isActive ? "active" : ""}><FaGraduationCap /> Formation</NavLink></li>
-        <li><NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}><FaFolderOpen /> Projets</NavLink></li>
+      <button
+        className="burger"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        ☰
+      </button>
+
+      <ul className={`nav-links ${isOpen ? "active" : ""}`}>
+        <li>
+          <NavLink to="/" onClick={() => setIsOpen(false)}>
+            <FaHome /> Accueil
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/formation" onClick={() => setIsOpen(false)}>
+            <FaGraduationCap /> Formation
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/projects" onClick={() => setIsOpen(false)}>
+            <FaFolderOpen /> Projets
+          </NavLink>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
