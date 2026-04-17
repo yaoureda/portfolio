7be9b44b6c170124a840d31f58c8af85d3bc21a0
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-04-18 00:57:45 +0200

    make weblink optional

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,7 +7,11 @@ function ProjectCard({ title, image, description, gitLink, webLink }) {
             <h3>{title}</h3>
             <p>{description}</p>
             <a href={gitLink} target="_blank">Lien GitHub</a> <br/>
-            <a href={webLink} target="_blank">Voir le site</a>
+            {webLink && (
+                <a href={webLink} target="_blank" rel="noopener noreferrer">
+                    Voir le site
+                </a>
+            )}
         </div>
     );
 }
