ae88a4a2532fe231994051c331b922ff1df9eebd
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-06-02 00:50:35 +0100

    add data for my current experiences

---
+++ src/data/experiences.js
@@ -0,0 +1,19 @@
+import ram from '../assets/ram.png'
+
+const experiences = [
+  {
+    id: 1,
+    title: "Stagiaire Développeur Full Stack à la Royal Air Maroc à Casablanca:",
+    image: ram,
+    durée: "Mai 2026 - Août 2026 (4 mois)",
+    description: [
+        "- Développement d'une plateforme de gestion et de suivi de bagages aériens.",
+        "- Conception et développement d'une API REST avec Spring Boot et PostgreSQL pour la gestion des bagages, vols et réclamations clients.",
+        "- Développement d'une application mobile en React Native permettant aux clients d'enregistrer leurs bagages, téléverser leurs photos, les associer à des vols, et déclarer leurs pertes.",
+        "- Développement d'une interface web en React et Typescript destinée au personnel afin de consulter les réclamations des clients et gérer leur traitement.",
+        "- Intégration d'un système d'authentification des clients et du personnel à l'aide de Spring Security."
+    ]
+  }
+]
+
+export default experiences
