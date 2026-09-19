adbda06fb49256ae9e189b29cca31135a1c23f05
YAOU Reda <reda.yaou@imt-atlantique.net>
2026-09-19 23:23:00 +0200

    add poketypes project information

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,5 +1,6 @@
 import todoappImg from '../assets/todoapp.png'
 import prepaflmaghrib from '../assets/prepaflmaghrib.png'
+import poketypes from '../assets/poketypes.png'
 import chatlobby from '../assets/chatlobby.png'
 
 const projects = [
@@ -13,6 +14,14 @@ const projects = [
   },
   {
     id: 2,
+    title: "PokeTypes",
+    image: poketypes,
+    description: "Un petit jeu consistant à deviner le type du Pokemon à partir de son sprite, développé avec Angular.",
+    gitLink: "https://github.com/yaoureda/poke-types",
+    webLink: "https://yaoureda.github.io/poke-types/"
+  },
+  {
+    id: 3,
     title: "To Do App",
     image: todoappImg,
     description: "Une application de gestion de tâches et de suivi d'habitudes. Elle comprend un système d'authentification et un dashboard de suivi des minutes travaillées durant les 7 derniers jours.",
@@ -20,7 +29,7 @@ const projects = [
     webLink: "https://to-do-app-y7i6.onrender.com/"
   },
   {
-    id: 3,
+    id: 4,
     title: "Prepa au Maroc",
     image: prepaflmaghrib,
     description: "Une plateforme web destinée aux élèves des CPGE MP du Maroc, proposant des informations générales sur les CPGE, un moteur de recherche d’annales du CNC (Concours national commun), et des conseils et ressources pour réussir sa prépa.",
